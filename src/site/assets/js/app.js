a350aee4c98ab4bde8bc5f5fa7100a8a5227ca7a
demirrtasfurkan <demirrtasfurkan@gmail.com>
2026-08-14 15:50:08 +0300

    app.js

--- src/site/assets/js/app.js
+++ src/site/assets/js/app.js
@@ -403,6 +403,6 @@
   }
   if ("serviceWorker" in navigator)
     window.addEventListener("load", () =>
-      navigator.serviceWorker.register("/sw.js").catch(() => {}),
+      navigator.serviceWorker.register("/sw.js?v=17").catch(() => {}),
     );
 })();
